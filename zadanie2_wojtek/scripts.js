2583857aa63e6a0753855bd53b7e5d1fcf45517b
wzyndul <wojteckiz8630@gmail.com>
2023-11-01 22:15:20 +0100

    [ADDED] step 4 and jquery

--- zadanie2_wojtek/scripts.js
+++ zadanie2_wojtek/scripts.js
@@ -1,80 +1,95 @@
 "use strict"
 let todoList = []; //declares a new array for Your todo list
-let initList = function () {
-    let savedList = window.localStorage.getItem("todos");
-    if (savedList != null)
-        todoList = JSON.parse(savedList);
-    else
-//code creating a default list with 2 items
-        todoList.push(
-            {
-                title: "Learn JS",
-                description: "Create a demo application for my TODO's",
-                place: "445",
-                dueDate: new Date(2019, 10, 16)
-            },
-            {
-                title: "Lecture test",
-                description: "Quick test from the first three lectures",
-                place: "F6",
-                dueDate: new Date(2019, 10, 17)
-            }
-            // of course the lecture test mentioned above will not take place
-        );
+
+
+const BASE_URL = "https://api.jsonbin.io/v3/b/6542a6d912a5d37659938cdb";
+const SECRET_KEY = "$2a$10$2nKbyBRgLZai1PueAEsk1uG/M8oqgBQIUexsjD7qFDf9Q.dNewgDe";
+$.ajax({
+    // copy Your bin identifier here. It can be obtained in the dashboard
+    url: BASE_URL,
+    type: 'GET',
+    headers: { //Required only if you are trying to access a private bin
+        'X-Master-Key': SECRET_KEY,
+        'X-Bin-Meta': false
+    },
+    success: (data) => {
+        // console.log(data);
+        todoList = data;
+    },
+    error: (err) => {
+        console.log(err.response);
+    }
+});
+
+let updateJSONbin = function () {
+    $.ajax({
+        url: BASE_URL,
+        type: 'PUT',
+        headers: { //Required only if you are trying to access a private bin
+            'X-Master-Key': SECRET_KEY
+        },
+        contentType: 'application/json',
+        data: JSON.stringify(todoList),
+        success: (data) => {
+            console.log(data);
+        },
+        error: (err) => {
+            console.log(err.response);
+        }
+    });
 }
 
-initList();
 
 let updateTodoList = function () {
-    let todoListDiv =
-        document.getElementById("todoListView");
+    let todoListDiv = $("#todoListView");
 
     //remove all elements
-    while (todoListDiv.firstChild) {
-        todoListDiv.removeChild(todoListDiv.firstChild);
-    }
+    todoListDiv.empty();
 
     //add all elements
-    let filterInput = document.getElementById("inputSearch");
-    for (let todo in todoList) {
-        if (
-            (filterInput.value == "") ||
-            (todoList[todo].title.includes(filterInput.value)) ||
-            (todoList[todo].description.includes(filterInput.value))
-        ) {
-            let newElement = document.createElement("p");
-            let newContent = document.createTextNode(todoList[todo].title + " " +
-                todoList[todo].description);
-            newElement.appendChild(newContent);
-            let newDeleteButton = document.createElement("input");
-            newDeleteButton.type = "button";
-            newDeleteButton.value = "x";
-            newDeleteButton.addEventListener("click",
-                function () {
-                    deleteTodo(todo);
-                });
-            newElement.appendChild(newDeleteButton);
-            todoListDiv.appendChild(newElement);
-        }
-    }
-}
+    let filterInput = $("#inputSearch");
 
+
+    $.each(todoList, function (index, todo) {
+        if (
+            (filterInput.val() === "") ||
+            (todo.title.includes(filterInput.val())) ||
+            (todo.description.includes(filterInput.val()))
+        ) {
+            let newElement = $("<p>" + todo.title + " " + todo.description + "</p>");
+
+            let newDeleteButton = $("<input type='button' value='x'>");
+            newDeleteButton.on("click", function () {
+                deleteTodo(index)
+            });
+
+            newElement.append(newDeleteButton);
+            todoListDiv.append(newElement);
+        }
+    });
+}
 setInterval(updateTodoList, 1000);
 
+// KROK 3C
 let deleteTodo = function (index) {
     todoList.splice(index, 1);
+    updateJSONbin();
 }
+
+// KROK 3B
 let addTodo = function () {
     //get the elements in the form
-    let inputTitle = document.getElementById("inputTitle");
-    let inputDescription = document.getElementById("inputDescription");
-    let inputPlace = document.getElementById("inputPlace");
-    let inputDate = document.getElementById("inputDate");
+    let inputTitle = $("#inputTitle");
+    let inputDescription = $("#inputDescription");
+    let inputPlace = $("#inputPlace");
+    let inputDate = $("#inputDate");
+
     //get the values from the form
-    let newTitle = inputTitle.value;
-    let newDescription = inputDescription.value;
-    let newPlace = inputPlace.value;
-    let newDate = new Date(inputDate.value);
+    let newTitle = inputTitle.val();
+    let newDescription = inputDescription.val();
+    let newPlace = inputPlace.val();
+    let newDate = new Date(inputDate.val());
+
     //create new item
     let newTodo = {
         title: newTitle,
@@ -82,7 +97,12 @@ let addTodo = function () {
         place: newPlace,
         dueDate: newDate
     };
+
     //add item to the list
     todoList.push(newTodo);
+
+    // KROK 3D
     window.localStorage.setItem("todos", JSON.stringify(todoList));
+
+    updateJSONbin();
 }
