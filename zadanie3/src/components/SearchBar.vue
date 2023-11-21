02d3843a76e63e285c0da5fc85af7abe039d19c0
wzyndul <wojteckiz8630@gmail.com>
2023-11-21 20:49:23 +0100

    [ADDED] SearchBar implementation

--- zadanie3/src/components/SearchBar.vue
+++ zadanie3/src/components/SearchBar.vue
@@ -1,10 +1,14 @@
 <script setup>
 // import {ref} from "vue";
+
 </script>
 
 <template>
   <div>
     <input v-model="searchFilter" type="text" placeholder="Search for a movie">
+    <input v-model="genreFilter" type="text" placeholder="Choose genre">
+    <input v-model="startDate" type="date" placeholder="Start Date">
+    <input v-model="endDate" type="date" placeholder="End Date">
     <button @click="searchMovie">Search</button>
   </div>
 </template>
