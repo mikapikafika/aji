597f5c22b28194c9533ee1448f1f854c0322594f
mwysocka <mikafikapika@gmail.com>
2024-01-12 17:02:03 +0100

    Added validation using Vuelidate

--- zadanie4/frontend/src/views/CheckoutForm.vue
+++ zadanie4/frontend/src/views/CheckoutForm.vue
@@ -1,7 +1,8 @@
 <script setup>
 import {computed, ref} from "vue";
 import {useStore} from "vuex";
-// import axios from "axios";
+import {useVuelidate} from "@vuelidate/core";
+import {required, email as emailValidator, minLength, numeric} from "@vuelidate/validators";
 
 const store = useStore();
 const userName = ref("");
@@ -9,21 +10,13 @@ const email = ref("");
 const phoneNumber = ref("");
 const orderedItems = computed(() => store.state.orderedItems);
 
-// Needed for input validation
-// const form = ref({
-//   userName: "",
-//   email: "",
-//   phoneNumber: "",
-// });
-//
-// const rules = {
-//   userName: {required},
-//   email: {required, email},
-//   phoneNumber: {required},
-// };
-//
-// const v$ = useVuelidate(rules);
+const rules = {
+  userName: { required },
+  email: { required, email: emailValidator },
+  phoneNumber: { required, numeric, minLength: minLength(9) }
+};
 
+const v$ = useVuelidate(rules, { userName, email, phoneNumber });
 
 const increaseQuantity = (index) => {
   orderedItems.value[index].Quantity++;
@@ -32,6 +25,8 @@ const increaseQuantity = (index) => {
 const decreaseQuantity = (index) => {
   if (orderedItems.value[index].Quantity > 1) {
     orderedItems.value[index].Quantity--;
+  } else {
+    removeProduct(index);
   }
 };
 
@@ -39,31 +34,30 @@ const removeProduct = (index) => {
   orderedItems.value.splice(index, 1);
 };
 
-// very much idk
-// const submitOrder = async () => {
-//   const order = {
-//     UserName: userName.value,
-//     Email: email.value,
-//     PhoneNumber: phoneNumber.value,
-//   };
-//
-//   try {
-//     const response = await axios.post("http://localhost:3000/orders", order);
-//     console.log(response.data);
-//     userName.value = "";
-//     email.value = "";
-//     phoneNumber.value = "";
-//     orderedItems.value = [];
-//   } catch (e) {
-//     console.log(e);
-//
-//   }
-// };
-
-
 const totalPrice = computed(() => {
   return orderedItems.value.reduce((total, product) => total + product.UnitPrice * product.Quantity, 0);
 });
+
+const submitOrder = () => {
+  if (v$.value.$invalid) {
+    alert('Invalid form');
+    return;
+  }
+
+  const order = {
+    UserName: userName.value,
+    Email: email.value,
+    PhoneNumber: phoneNumber.value,
+    OrderedItems: orderedItems.value.map(product => {
+      return {
+        ProductId: product.ProductId,
+        Quantity: product.Quantity
+      };
+    })
+  };
+
+  console.log(order);
+};
 </script>
 
 <template>
@@ -98,15 +92,18 @@ const totalPrice = computed(() => {
     <form @submit.prevent="submitOrder">
       <label>
         Username:
-        <input v-model="userName" type="text" required/>
+        <input v-model="userName" type="text"/>
+        <span v-if="v$.userName.$error">Username is required</span>
       </label>
       <label>
         Email:
-        <input v-model="email" type="email" required/>
+        <input v-model="email" type="email"/>
+        <span v-if="v$.email.$error">Email is required</span>
       </label>
       <label>
         Phone:
-        <input v-model="phoneNumber" type="tel" required/>
+        <input v-model="phoneNumber" type="tel"/>
+        <span v-if="v$.phoneNumber.$error">Phone number is required</span>
       </label>
       <button type="submit">Submit Order</button>
     </form>
